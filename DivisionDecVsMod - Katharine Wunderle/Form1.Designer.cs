811b6aecfd499bf906d2d741be077823167fcb51
Katie <95194045+kwunderle@users.noreply.github.com>
2022-08-18 21:13:33 -0400

    Add files via upload

---
+++ DivisionDecVsMod - Katharine Wunderle/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace DivisionDecVsMod___Katharine_Wunderle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dividendLbl = new System.Windows.Forms.Label();
+            this.divisorLbl = new System.Windows.Forms.Label();
+            this.dividedByLbl = new System.Windows.Forms.Label();
+            this.divisorInpt = new System.Windows.Forms.TextBox();
+            this.dividendInpt = new System.Windows.Forms.TextBox();
+            this.answrModLbl = new System.Windows.Forms.Label();
+            this.quotientLbl = new System.Windows.Forms.Label();
+            this.quotientOtpt = new System.Windows.Forms.Label();
+            this.remainderLbl = new System.Windows.Forms.Label();
+            this.remainderOtpt = new System.Windows.Forms.Label();
+            this.answrDecLbl = new System.Windows.Forms.Label();
+            this.decOtpt = new System.Windows.Forms.Label();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.clrBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dividendLbl
+            // 
+            this.dividendLbl.AutoSize = true;
+            this.dividendLbl.Location = new System.Drawing.Point(42, 18);
+            this.dividendLbl.Name = "dividendLbl";
+            this.dividendLbl.Size = new System.Drawing.Size(52, 13);
+            this.dividendLbl.TabIndex = 0;
+            this.dividendLbl.Text = "Dividend:";
+            // 
+            // divisorLbl
+            // 
+            this.divisorLbl.AutoSize = true;
+            this.divisorLbl.Location = new System.Drawing.Point(189, 18);
+            this.divisorLbl.Name = "divisorLbl";
+            this.divisorLbl.Size = new System.Drawing.Size(42, 13);
+            this.divisorLbl.TabIndex = 1;
+            this.divisorLbl.Text = "Divisor:";
+            // 
+            // dividedByLbl
+            // 
+            this.dividedByLbl.AutoSize = true;
+            this.dividedByLbl.Location = new System.Drawing.Point(111, 37);
+            this.dividedByLbl.Name = "dividedByLbl";
+            this.dividedByLbl.Size = new System.Drawing.Size(57, 13);
+            this.dividedByLbl.TabIndex = 2;
+            this.dividedByLbl.Text = "Divided by";
+            // 
+            // divisorInpt
+            // 
+            this.divisorInpt.Location = new System.Drawing.Point(171, 34);
+            this.divisorInpt.Name = "divisorInpt";
+            this.divisorInpt.Size = new System.Drawing.Size(75, 20);
+            this.divisorInpt.TabIndex = 2;
+            // 
+            // dividendInpt
+            // 
+            this.dividendInpt.Location = new System.Drawing.Point(30, 34);
+            this.dividendInpt.Name = "dividendInpt";
+            this.dividendInpt.Size = new System.Drawing.Size(75, 20);
+            this.dividendInpt.TabIndex = 1;
+            // 
+            // answrModLbl
+            // 
+            this.answrModLbl.AutoSize = true;
+            this.answrModLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answrModLbl.Location = new System.Drawing.Point(42, 135);
+            this.answrModLbl.Name = "answrModLbl";
+            this.answrModLbl.Size = new System.Drawing.Size(199, 13);
+            this.answrModLbl.TabIndex = 5;
+            this.answrModLbl.Text = "Answer Via Integer Division (Mod)";
+            // 
+            // quotientLbl
+            // 
+            this.quotientLbl.AutoSize = true;
+            this.quotientLbl.Location = new System.Drawing.Point(21, 163);
+            this.quotientLbl.Name = "quotientLbl";
+            this.quotientLbl.Size = new System.Drawing.Size(50, 13);
+            this.quotientLbl.TabIndex = 6;
+            this.quotientLbl.Text = "Quotient:";
+            // 
+            // quotientOtpt
+            // 
+            this.quotientOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.quotientOtpt.Location = new System.Drawing.Point(77, 158);
+            this.quotientOtpt.Name = "quotientOtpt";
+            this.quotientOtpt.Size = new System.Drawing.Size(47, 23);
+            this.quotientOtpt.TabIndex = 7;
+            this.quotientOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // remainderLbl
+            // 
+            this.remainderLbl.AutoSize = true;
+            this.remainderLbl.Location = new System.Drawing.Point(141, 163);
+            this.remainderLbl.Name = "remainderLbl";
+            this.remainderLbl.Size = new System.Drawing.Size(61, 13);
+            this.remainderLbl.TabIndex = 8;
+            this.remainderLbl.Text = "Remainder:";
+            // 
+            // remainderOtpt
+            // 
+            this.remainderOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.remainderOtpt.Location = new System.Drawing.Point(208, 158);
+            this.remainderOtpt.Name = "remainderOtpt";
+            this.remainderOtpt.Size = new System.Drawing.Size(47, 23);
+            this.remainderOtpt.TabIndex = 9;
+            this.remainderOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // answrDecLbl
+            // 
+            this.answrDecLbl.AutoSize = true;
+            this.answrDecLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answrDecLbl.Location = new System.Drawing.Point(59, 204);
+            this.answrDecLbl.Name = "answrDecLbl";
+            this.answrDecLbl.Size = new System.Drawing.Size(172, 13);
+            this.answrDecLbl.TabIndex = 10;
+            this.answrDecLbl.Text = "Answer Via Decimal Devision";
+            // 
+            // decOtpt
+            // 
+            this.decOtpt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.decOtpt.Location = new System.Drawing.Point(90, 229);
+            this.decOtpt.Name = "decOtpt";
+            this.decOtpt.Size = new System.Drawing.Size(100, 23);
+            this.decOtpt.TabIndex = 11;
+            this.decOtpt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(77, 72);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(125, 23);
+            this.calcBtn.TabIndex = 3;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // clrBtn
+            // 
+            this.clrBtn.Location = new System.Drawing.Point(102, 269);
+            this.clrBtn.Name = "clrBtn";
+            this.clrBtn.Size = new System.Drawing.Size(75, 23);
+            this.clrBtn.TabIndex = 13;
+            this.clrBtn.Text = "Clear";
+            this.clrBtn.UseVisualStyleBackColor = true;
+            this.clrBtn.Click += new System.EventHandler(this.clrBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 304);
+            this.Controls.Add(this.clrBtn);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.decOtpt);
+            this.Controls.Add(this.answrDecLbl);
+            this.Controls.Add(this.remainderOtpt);
+            this.Controls.Add(this.remainderLbl);
+            this.Controls.Add(this.quotientOtpt);
+            this.Controls.Add(this.quotientLbl);
+            this.Controls.Add(this.answrModLbl);
+            this.Controls.Add(this.dividendInpt);
+            this.Controls.Add(this.divisorInpt);
+            this.Controls.Add(this.dividedByLbl);
+            this.Controls.Add(this.divisorLbl);
+            this.Controls.Add(this.dividendLbl);
+            this.Name = "Form1";
+            this.Text = "Division in C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label dividendLbl;
+        private System.Windows.Forms.Label divisorLbl;
+        private System.Windows.Forms.Label dividedByLbl;
+        private System.Windows.Forms.TextBox divisorInpt;
+        private System.Windows.Forms.TextBox dividendInpt;
+        private System.Windows.Forms.Label answrModLbl;
+        private System.Windows.Forms.Label quotientLbl;
+        private System.Windows.Forms.Label quotientOtpt;
+        private System.Windows.Forms.Label remainderLbl;
+        private System.Windows.Forms.Label remainderOtpt;
+        private System.Windows.Forms.Label answrDecLbl;
+        private System.Windows.Forms.Label decOtpt;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Button clrBtn;
+    }
+}
+
---
+++ DivisionDecVsMod - Katharine Wunderle/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DivisionDecVsMod___Katharine_Wunderle
+{
+    //Author: Katharine Wunderle
+    //ID: 623748
+    //Date: 04/11/2021
+    //Goal: Demonstrate division using decimal and modulo division
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            //VARIABLES
+            //Input Variables
+            decimal dividend = 0;
+            decimal divisor = 0;
+            bool tryParseResult;
+            //Output Variables
+            int myQuotient = 0, myRemainder = 0;
+            decimal decimalOtpt = 0;
+            //INPUT
+            //Get dividend and divisor and verify numeric value from user
+            //Verify dividend is numeric
+            tryParseResult = decimal.TryParse(dividendInpt.Text, out dividend);
+            if (tryParseResult == false)
+            { MessageBox.Show("Error: Please enter a numeric dividend.");
+                return; }
+            //Verify divisor is numeric
+            tryParseResult = decimal.TryParse(divisorInpt.Text, out divisor);
+            if (tryParseResult == false || divisor == 0)
+            { MessageBox.Show("Error: Please enter a numeric divisor that isn't 0.");
+                return;
+            }
+            //CALCULATIONS
+            //Calculate Quotient
+            myQuotient = (int)dividend / (int)divisor;
+            //Calculate Remainder
+            myRemainder = (int)dividend % (int)divisor;
+            //Calculate Decimal value
+            decimalOtpt = dividend / divisor;
+
+            //OUTPUT
+            //Display Quotient
+            quotientOtpt.Text = myQuotient.ToString();
+            //Display Remainder
+            remainderOtpt.Text = myRemainder.ToString();
+            //Display Decimal value
+            decOtpt.Text = decimalOtpt.ToString("n4");
+
+        }
+
+        private void clrBtn_Click(object sender, EventArgs e)
+        {
+            //Clear input textboxes
+            dividendInpt.Text = "";
+            divisorInpt.Text = "";
+            //Clear Output textboxes
+            quotientOtpt.Text = "";
+            remainderOtpt.Text = "";
+            decOtpt.Text = "";
+        }
+    }
+}
